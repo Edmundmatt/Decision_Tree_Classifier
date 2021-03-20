49b6235c9e5b9555242a89687f0966101dd12377
edmundmatt <edmundmatt@myvuw.ac.nz>
2021-03-21 09:19:57 +1300

    Initial commit

---
+++ src/DecisionTree.java
@@ -0,0 +1,8 @@
+public class DecisionTree {
+
+    public static void main(String[] args){
+        FileManager fileManager = new FileManager();
+        fileManager.read("C:\\Users\\Matthew\\IdeaProjects\\COMP307Assignment1Part2\\data\\hepatitis-training");
+
+    }
+}
