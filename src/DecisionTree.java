b845e5a159dcbe0f7fc1ce0ae897b6e7f33a4ebe
Matthew Edmundson <edmundmatt@ecs.vuw.ac.nz>
2021-03-23 15:30:06 +1300

    Reformatted the class structure.

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,13 +1,21 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class DecisionTree {
+    int numCategories;
+    int numAtts;
+    List<String> categoryNames;
+    List<String> attNames;
+    List<HepInstance> allInstances;
 
+    public DecisionTree(String fileName, Boolean completedTree){
+//        read("C:\\Users\\Matthew\\IdeaProjects\\COMP307Assignment1Part2\\data\\hepatitis-training");
+        read(fileName);
+    }
     public static void main(String[] args){
-        FileManager fMTraining = new FileManager();
-        fMTraining.read("C:\\Users\\Matthew\\IdeaProjects\\COMP307Assignment1Part2\\data\\hepatitis-training");
-        FileManager fMTest = new FileManager();
-        fMTest.read("C:\\Users\\Matthew\\IdeaProjects\\COMP307Assignment1Part2\\data\\hepatitis-test");
 
     }
 
@@ -35,7 +43,7 @@ public class DecisionTree {
                 //if(weighted av. purity of the sets is best so far)
                     //bestAtt = this attribute
                     //bestInstsTrue = set of true instances
-                    //besInstsFalse = set of flase instances
+                    //besInstsFalse = set of false instances
             //Build subTrees using remaining the remaining attributes
             //Node left = buildTree(bestInstsTrue, attributes - bestAtt)
             //Node right = buildTree(bestInstsFalse, attributes - bestAtt)
@@ -51,11 +59,11 @@ public class DecisionTree {
      */
     private static boolean isPure(List<Node> instances){
 
-//        return instances.stream()
-//                .map(HepInstance::getCategory)
-//                .distinct()
-//                .limit(2)
-//                .count() <= 1;
+        return instances.stream()
+                .map(HepInstance::getCategory)
+                .distinct()
+                .limit(2)
+                .count() <= 1;
         return true;
     }
 
@@ -66,4 +74,75 @@ public class DecisionTree {
 
         return purity;
     }
+
+    private static void highestProb(){
+
+    }
+
+    /**-----------------------------------------------------------*/
+
+    private void read(String fileName){
+        System.out.println("Reading data from file: "+fileName);
+        try{
+            Scanner din = new Scanner(new File(fileName));
+
+            categoryNames = new ArrayList<>();
+            for (Scanner s = new Scanner(din.nextLine()); s.hasNext();) categoryNames.add(s.next());
+            numCategories=categoryNames.size();
+            System.out.println(numCategories +" categories");
+
+            attNames = new ArrayList<>();
+            for (Scanner s = new Scanner(din.nextLine()); s.hasNext();) attNames.add(s.next());
+            numAtts = attNames.size();
+            System.out.println(numAtts +" attributes");
+
+            allInstances = readInstances(din);
+            din.close();
+
+        }catch(IOException e){
+            throw new RuntimeException("Data File caused IOException");
+        }
+    }
+
+    private List<HepInstance> readInstances(Scanner din){
+        /* instance = classname and space separated attribute values */
+        List<HepInstance> hepInstances = new ArrayList<>();
+        while (din.hasNext()){
+            Scanner line = new Scanner(din.nextLine());
+            hepInstances.add(new HepInstance(categoryNames.indexOf(line.next()),line));
+        }
+        System.out.println("Read " + hepInstances.size()+" instances");
+        return hepInstances;
+    }
+
+    private class HepInstance {
+        //    private final List<String> categoryNames;
+        private final int category;
+        private final List<Boolean> values;
+
+        public HepInstance(int category, Scanner s){
+            this.category = category;
+            this.values = new ArrayList<>();
+            while (s.hasNextBoolean()) values.add(s.nextBoolean());
+        }
+
+        public boolean getAtt(int index){
+            return values.get(index);
+        }
+
+        public int getCategory(){
+            return category;
+        }
+
+    public String toString(){
+        StringBuilder ans = new StringBuilder(categoryNames.get(category));
+        ans.append(" ");
+        for (Boolean val : values)
+            ans.append(val?"true  ":"false ");
+        return ans.toString();
+    }
+
+    }
+
 }
+
--- src/FileManager.java
+++ src/FileManager.java
@@ -5,43 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FileManager {
-    int numCategories;
-    int numAtts;
-    List<String> categoryNames;
-    List<String> attNames;
-    List<HepInstance> allInstances;
 
-    public void read(String fileName){
-        System.out.println("Reading data from file: "+fileName);
-        try{
-            Scanner din = new Scanner(new File(fileName));
 
-            categoryNames = new ArrayList<>();
-            for (Scanner s = new Scanner(din.nextLine()); s.hasNext();) categoryNames.add(s.next());
-            numCategories=categoryNames.size();
-            System.out.println(numCategories +" categories");
 
-            attNames = new ArrayList<>();
-            for (Scanner s = new Scanner(din.nextLine()); s.hasNext();) attNames.add(s.next());
-            numAtts = attNames.size();
-            System.out.println(numAtts +" attributes");
-
-            allInstances = readInstances(din);
-            din.close();
-
-        }catch(IOException e){
-            throw new RuntimeException("Data File caused IOException");
-        }
-    }
-
-    private List<HepInstance> readInstances(Scanner din){
-        /* instance = classname and space separated attribute values */
-        List<HepInstance> hepInstances = new ArrayList<>();
-        while (din.hasNext()){
-            Scanner line = new Scanner(din.nextLine());
-            hepInstances.add(new HepInstance(categoryNames.indexOf(line.next()),line));
-        }
-        System.out.println("Read " + hepInstances.size()+" instances");
-        return hepInstances;
-    }
 }
